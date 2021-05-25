77334cac4f5b4b92ce817d0283b90a39af53c70e
samuel <samuel_xiangpeng@163.com>
2021-05-26 00:27:02 +0800

    :tada:页面部署

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/css/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Resume from './Resume'
 
 ReactDOM.render(
-  <React.StrictMode>
     <HashRouter>
       <Switch>
         <Route path='/' component={ App } exact />
         <Route path='/resume' component={ Resume } />
       </Switch>
-    </HashRouter>
-  </React.StrictMode>,
+    </HashRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
